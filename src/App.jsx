496592051dde1a598a54db64953062ed69e31f7f
meifinatul <meifina138@gmail.com>
2024-07-06 17:56:29 +0700

    pertama kali

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import {StyleSheet, Text, View, Flatlist} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Beranda from './screens/Beranda';
+import Makanan from './screens/Makanan';
+import Profil from './screens/Profil';
+
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const MenuTab = () => {
+  return (
+    <Tabs.Navigator screenOptions={{tabBarActiveTintColor: 'green'}}>
+      <Tabs.Screen
+        name="Beranda"
+        component={Beranda}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Diskon"
+        component={Makanan}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons name="sale" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profil"
+        component={Profil}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons name="account" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tab"
+          component={MenuTab}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+const style = StyleSheet.create({});
